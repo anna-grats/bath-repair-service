ecc42721f4bfbd74033a852677eb6550f8d1475a
Anna Grats <anna.gratss@gmail.com>
2025-04-22 14:55:11 -0500

    Install package.json

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,16 +1,16 @@
 import Header from '@/sections/Header/Header';
 import { getDictionary } from './dictionaries/dictionaries';
-import Hero from '@/sections/Hero/Hero';
-import Certified from '@/sections/Certified/Certified';
-import Benefits from '@/sections/Benefits/Benefits';
-import Compare from '@/sections/Compare/Compare';
-import Reviews from '@/sections/Reviews/Reviews';
-import Works from '@/sections/Works/Works';
-import ContactForm from '@/sections/ContactForm/ContactForm';
-import Footer from '@/sections/Footer/Footer';
-import Calculator from '@/sections/Calculator/Calculator';
-import ScrollToTop from '@/components/Top/top';
-import FAQ from '@/components/FAQ/FAQ';
+import Hero from '../../sections/Hero/Hero';
+import Certified from '../../sections/Certified/Certified';
+import Benefits from '../../sections/Benefits/Benefits';
+import Compare from '../../sections/Compare/Compare';
+import Reviews from '../../sections/Reviews/Reviews';
+import Works from '../../sections/Works/Works';
+import ContactForm from '../../sections/ContactForm/ContactForm';
+import Footer from '../../sections/Footer/Footer';
+import Calculator from '../../sections/Calculator/Calculator';
+import ScrollToTop from '../../components/Top/top';
+import FAQ from '../../components/FAQ/FAQ';
 
 
 
